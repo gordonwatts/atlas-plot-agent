ab305032946a352c10c2e9a91df225920c417bee
Gordon Watts <gwatts@uw.edu>
2025-06-25 22:38:14 -0700

    Get output types to work, turn off other agents.

--- atlas_plot_agent/datamodel.py
+++ atlas_plot_agent/datamodel.py
@@ -1,11 +1,12 @@
 from dataclasses import dataclass
+from typing import Optional
 
 
 @dataclass
 class dataset_reference:
     "A single dataset"
 
-    # The full rucio (for now) dataset name
+    # The full rucio dataset name
     name: str
 
 
@@ -13,4 +14,20 @@ class dataset_reference:
 class conversation_context:
     "Context used to pass between tools"
 
+    # The dataset we will pull data from
     ds: dataset_reference
+
+    # What we want to plot that is in the dataset.
+    what_to_plot: str
+
+
+def dataclass_type_from_string(type_str: Optional[str]):
+    "Convert a string to a dataclass type"
+    if type_str is None:
+        return None
+    if type_str == "dataset_reference":
+        return dataset_reference
+    elif type_str == "conversation_context":
+        return conversation_context
+    else:
+        raise ValueError(f"Unknown type: {type_str}")
